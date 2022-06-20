b7ae9d483f94039a3c9214512c954c33ff14290a
stephen <stevedogbey@gmail.com>
2022-06-20 16:45:16 +0000

    lap completed

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+function writeCards(names, birthday){
+    let newArray = [];
+    for(let counter = 0; counter < names.length; counter++){
+        newArray.push(`Thank you, ${names[counter]}, for the wonderful ${birthday} gift!`)
+    }
+    return newArray;
+
+}
+writeCards(names, "surprise");
+
+
+
+
+function countDown(number){
+    let i = -1;
+    while(number > i){
+        console.log(number)
+        number--;
+    }
+}
+countDown(10)
